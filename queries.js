701d8ecff6970f0b35131f095abfa4449fde2d8e
Jose Moreno <josemoreno@Joses-MacBook-Air.local>
2017-05-16 09:53:50 -0700

    Add delete

--- queries.js
+++ queries.js
@@ -34,8 +34,8 @@ let createTodo = (req, res, next) => {
     })
     .catch(err => {
       return next(err);
-    });
-}
+    })
+};
 
 let updateTodo = (req, res, next) => {
   db.none('update todos set description = $1, status = $2, due = $3 WHERE id = $4', [req.body.description, req.body.status, req.body.due, req.params.id])
@@ -49,11 +49,29 @@ let updateTodo = (req, res, next) => {
     .catch(err => {
       return next(err);
     })
-}
+};
+
+let deleteTodo = (req, res, next) => {
+  db.result('DELETE FROM todos WHERE id = $1', req.params.id)
+    .then(result => {
+      res.status(200)
+      .json({
+        status: 'success',
+        message: `Removed ${result.rowCount} todo`
+      })
+      console.log("Result " + result.rowCount);
+      console.log("This is the result" + result);
+    })
+    .catch(err => {
+      return next(err);
+    })
+};
+
 
 
 module.exports = {
   getAllTodos: getAllTodos,
   createTodo: createTodo,
-  updateTodo: updateTodo
+  updateTodo: updateTodo,
+  deleteTodo: deleteTodo
 };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const db = require('../queries');
 router.get('/api/todos', db.getAllTodos);
 router.post('/api/todos', db.createTodo);
 router.put('/api/todos/:id', db.updateTodo);
-
+router.delete('/api/todos/:id', db.deleteTodo);
 
 module.exports = router;
