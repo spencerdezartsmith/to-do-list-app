de2dc81ab83153a8742f5152656278bfc498f3f3
Jose Moreno <josemoreno@Joses-MacBook-Air.local>
2017-05-15 17:09:31 -0700

    Add home route /api/todos

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const db = require('../queries');
+
+router.get('/api/todos', db.getAllTodos);
+
 
 module.exports = router;
