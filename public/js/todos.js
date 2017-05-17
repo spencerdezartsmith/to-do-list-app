5c106eec767598f5776781c2d56ae97d776142d5
Jose Moreno <josemoreno@Joses-MacBook-Air.local>
2017-05-17 16:49:29 -0700

    Setup add todo modal

---
+++ public/js/todos.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  $('#dialog').dialog({
+    title: 'Add New Todo',
+    autoOpen: false,
+    modal: true,
+    show: {
+      effect: 'blind',
+      duration: 1000
+    },
+    hide: {
+      effect: 'blind',
+      duration: 1000
+    },
+    buttons: [{
+      text: 'Add',
+      click: function() {
+        $(this).dialog('close')
+      }
+    }]
+  });
+
+  $('#opener').click(() => {
+    $('#dialog').dialog('open');
+  })
+})
